ab7535960e405db69452aa3e3b20cf0949363ebb
Devaj Suryavanshi <2021mt70003@wilp.bits-pilani.ac.in>
2025-03-01 01:38:13 +0530

    Daily Practice

--- Sorting.java
+++ Sorting.java
@@ -35,6 +35,47 @@ public class Sorting {
         }
         return arr;
     }
+    
+    public static int[] mergeSort(int[] arr, int start, int end){ //The solution can be more optimised if we don't create many array and only play with indexes in both function
+
+        // Array with 1 element always sorted
+        if(start == end-1)
+            return new int[]{arr[start]};
+
+        int mid = (start+end)/2; //Dividing Array into 2 Parts
+
+        int[] left = mergeSort(arr, start, mid); //Left array
+        int[] right = mergeSort(arr, mid, end); //Right array
+        
+        return merge(left,right); //Merging Left and Right into a sorted array
+        
+    }
+
+    public static int[] merge(int[] left, int[] right){
+        int merged[] = new int[left.length + right.length]; //Merged array Length
+        int i = 0, k = 0, j= 0; // i is for left array counter and k is inserting element into merged and j is for right array counter
+        while(i < left.length && j < right.length){
+            if(left[i] < right[j]){ //if left is smaller then left element will be inserted and left count will increase else right
+                merged[k] = left[i];
+                i++;
+            } else {
+                merged[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < left.length){ //if left array elements are left then they will be simply inserted in remaining spaces since they are already sorted
+            merged[k] = left[i];
+            i++;
+            k++;
+        }
+        while(j <  right.length){ //same if right array elements are left
+            merged[k] = right[j];
+            j++;
+            k++;
+        }
+        return merged; // finally returning merged array
+    }
 
     public static int[] selectionSort(int[] arr){
 
@@ -54,7 +95,9 @@ public class Sorting {
 
     public static void main(String[] args) {
         int[] arr = {4,55,121,2,3,5,9,66};
-        System.out.println(Arrays.toString(insertionSort(arr)));
+        //System.out.println(Arrays.toString(insertionSort(arr)));
+
+        System.out.println(Arrays.toString(mergeSort(arr, 0, arr.length)));
     }
     
 }
